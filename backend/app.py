df3d05b4bb2869f0b458bdf69da569f2dcf21fe7
Arya <ary.saw.rt23@dypatil.edu>
2026-07-07 20:36:50 +0530

    Completed Day 4 - Parsed Pylint output into structured JSON

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,58 @@ from flask import Flask, request, jsonify
 import subprocess
 import os
 import sys
+import re
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "AI Code Review Backend Running"
+
+def parse_pylint_output(output):
+
+    issues = []
+
+    score = "Not Available"
+
+    score_match = re.search(r"rated at ([0-9.]+/10)", output)
+
+    if score_match:
+        score = score_match.group(1)
+
+    pattern = r".*temp_code\.py:(\d+):\d+:\s([A-Z]\d+):\s(.*)\s\((.*)\)"
+
+    matches = re.findall(pattern, output)
+
+    for match in matches:
+
+        line = int(match[0])
+
+        code = match[1]
+
+        message = match[2]
+
+        issue_type = match[3]
+
+        issues.append({
+
+            "line": line,
+
+            "code": code,
+
+            "type": issue_type,
+
+            "message": message
+
+        })
+
+    return {
+
+        "score": score,
+
+        "issues": issues
+
+    }
 
 @app.route("/review", methods=["POST"])
 def review_code():
@@ -35,9 +81,10 @@ def review_code():
     text=True
 )
 
-    return jsonify({
-    "review": result.stdout,
-    "return_code": result.returncode
-})
+    print(result.stdout)
+    
+    parsed_review = parse_pylint_output(result.stdout)
+
+    return jsonify(parsed_review)
 if __name__ == "__main__":
     app.run(debug=False)
