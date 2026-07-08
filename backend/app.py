89be539b26d9e8feeb5a270ba82f8cb84905d6b2
Arya <ary.saw.rt23@dypatil.edu>
2026-07-08 20:34:18 +0530

    Completed Day 5 - Added issue summary and categorization

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,13 @@ def parse_pylint_output(output):
 
     issues = []
 
+    summary = {
+        "Convention": 0,
+        "Warning": 0,
+        "Error": 0,
+        "Refactor": 0
+    }
+
     score = "Not Available"
 
     score_match = re.search(r"rated at ([0-9.]+/10)", output)
@@ -23,36 +30,39 @@ def parse_pylint_output(output):
 
     pattern = r".*temp_code\.py:(\d+):\d+:\s([A-Z]\d+):\s(.*)\s\((.*)\)"
 
-    matches = re.findall(pattern, output)
-
+    matches = re.findall(pattern, output, re.MULTILINE)
+    
     for match in matches:
 
         line = int(match[0])
-
         code = match[1]
-
         message = match[2]
-
         issue_type = match[3]
 
+        if code.startswith("C"):
+            summary["Convention"] += 1
+
+        elif code.startswith("W"):
+            summary["Warning"] += 1
+
+        elif code.startswith("E"):
+            summary["Error"] += 1
+
+        elif code.startswith("R"):
+            summary["Refactor"] += 1
+
         issues.append({
-
             "line": line,
-
             "code": code,
-
             "type": issue_type,
-
             "message": message
-
         })
 
     return {
-
         "score": score,
-
+        "total_issues": len(issues),
+        "summary": summary,
         "issues": issues
-
     }
 
 @app.route("/review", methods=["POST"])
@@ -75,16 +85,26 @@ def review_code():
     with open(filename, "w") as file:
         file.write(code)
 
+        print("Python Executable:", sys.executable)
+
     result = subprocess.run(
     [sys.executable, "-m", "pylint", filename],
     capture_output=True,
     text=True
 )
 
+    print("Pylint Return Code:", result.returncode)
+    print("STDERR:", result.stderr)
+
+    print("STDOUT:")
     print(result.stdout)
+
+    print("STDERR:")
+    print(result.stderr)
     
     parsed_review = parse_pylint_output(result.stdout)
 
     return jsonify(parsed_review)
+
 if __name__ == "__main__":
     app.run(debug=False)
